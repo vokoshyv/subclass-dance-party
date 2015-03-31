f333fb19f7ecbc16e26f68a66a1339ca8a2a2fd5
vokoshyv <ronsuij@gmail.com>
2015-03-30 19:07:14 -0700

    removed debugger

--- src/init.js
+++ src/init.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
      * A new object of the given type will be created and added
      * to the stage.
      */
-  debugger;
+
     var dancerMakerFunctionName = $(this).data("dancer");
 
     // get the maker function for the kind of dancer we're supposed to make
